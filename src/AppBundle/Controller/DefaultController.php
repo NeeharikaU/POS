cd09e9831e4284c40f75a52d10d427d6f3f57bc1
Camaria Bevavy <bcamaria@gmail.com>
2015-05-12 12:27:20 -0400

    install default page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,10 +8,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/", name="homepage")
      */
     public function indexAction()
     {
         return $this->render('default/index.html.twig');
+    }
+    
+    /**
+     * @Route("/product", name="product")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function productAction()
+    {
+    	return $this->render('default/products.html.twig', array('products' => array(
+    			array(
+    			'id' => 1234950305,
+    			'name' => "dslkjkffkflsflflf",
+    			'category' => '',
+    			'quantity' => 7,
+    					'price' => 10
+    					),
+    			array(
+    					'id' => 1234950305,
+    					'name' => "dslkjkffkflsflflf",
+    					'category' => '',
+    					'quantity' => 7,
+    					'price' => 19
+    			)
+    	)));
     }
 }
